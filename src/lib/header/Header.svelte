f599c9d56d07c41ebbbefe5a130c4aea36a2b826
Sanjay M <arjunsanjay0@gmail.com>
2022-08-31 03:28:11 +0530

    added tailwindcss

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -24,7 +24,6 @@
 			<li class:active={$page.url.pathname === '/todos'}>
 				<a data-sveltekit-prefetch href="/todos">Todos</a>
 			</li>
-		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
