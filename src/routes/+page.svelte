14f277e6c000d843497f6a67d0a03c6a31f4dc55
Sanjay M <arjunsanjay0@gmail.com>
2022-08-31 04:36:07 +0530

    added event list card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<h2 class="text-3xl font-bold ">Setup tailwindcss</h2>
+<script></script>
+
+<main class="bg-gray-200 h-screen ">
+	<div class="container m-auto py-10">
+		<h2 class="text-4xl font-bold ">UFC Fans</h2>
+		<div class="rounded-lg shadow bg-gray-300 p-12 mt-48 relative">
+			<img
+				src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2021-06/71544%252Fprofile-galery%252Ffullbodyleft-picture%252FGANE_CYRIL_L_06-26.png?itok=MA7V_Rju"
+				alt=""
+				class="h-80 absolute bottom-0 left-0"
+			/>
+			<div class="text-center">
+				<p class="text-3xl font-semibold">UFC Fight Night: Gane vs. Tuivasa</p>
+				<p class="text-gray-600">September 03, 2022</p>
+				<p class="text-lg">Paris, Ile-de-France, France</p>
+			</div>
+			<img
+				src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2022-03/024162f6-408f-4518-8855-f055df3f5da8%252FTUIVASA_TAI_L_02-12.png?itok=3oinmKUW"
+				alt=""
+				class="h-80 absolute bottom-0 right-0"
+			/>
+		</div>
+
+    
+	</div>
+</main>
