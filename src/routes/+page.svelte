b63ceb64a393faa50162040e661b682d0334c8ab
Sanjay M <arjunsanjay0@gmail.com>
2022-08-31 23:48:19 +0530

    added colour for name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,16 @@
 
 <main class="bg-gray-200 h-screen ">
 	<div class="container m-auto py-10">
-		<h2 class="text-4xl font-bold ">UFC Fans</h2>
-		<div class="rounded-lg shadow bg-gray-300 p-12 mt-48 relative">
+		<h1 href="/" class="text-4xl font-bold "><a class="text-ufc-red" href="/">UFC </a>Fans</h1>
+		<div class="rounded-lg shadow bg-gray-300 p-10 mt-48 relative">
 			<img
 				src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2021-06/71544%252Fprofile-galery%252Ffullbodyleft-picture%252FGANE_CYRIL_L_06-26.png?itok=MA7V_Rju"
 				alt=""
 				class="h-80 absolute bottom-0 left-0"
 			/>
 			<div class="text-center">
-				<p class="text-3xl font-semibold">UFC Fight Night: Gane vs. Tuivasa</p>
-				<p class="text-gray-600">September 03, 2022</p>
+				<p class="text-3xl font-semibold text-ufc-red">UFC Fight Night: Gane vs. Tuivasa</p>
+				<p class="text-gray-600 my-2">September 03, 2022</p>
 				<p class="text-lg">Paris, Ile-de-France, France</p>
 			</div>
 			<img
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'ufc-red': '#D51C1C'
+			}
+		}
 	},
 	plugins: []
 };
