dc3abedd0f540c9689051065e8b11c5144e7b88e
Sanjay M <arjunsanjay0@gmail.com>
2022-09-03 20:13:51 +0530

    displaying all events on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,30 @@
-<script></script>
+<script>
+	import { onMount } from 'svelte';
 
-<main class="bg-gray-200 h-screen ">
+	let data = [];
+	onMount(async () => {
+		const res = await fetch('http://localhost:8000/upcoming-events');
+
+		const results = await res.json();
+		data = results;
+		console.log(results);
+	});
+</script>
+
+<main class="bg-gray-200 h-full ">
 	<div class="container m-auto py-10">
 		<h1 href="/" class="text-4xl font-bold "><a class="text-ufc-red" href="/">UFC </a>Fans</h1>
-		<div class="rounded-lg shadow bg-gray-300 p-10 mt-48 relative">
-			<img
-				src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2021-06/71544%252Fprofile-galery%252Ffullbodyleft-picture%252FGANE_CYRIL_L_06-26.png?itok=MA7V_Rju"
-				alt=""
-				class="h-80 absolute bottom-0 left-0"
-			/>
-			<div class="text-center">
-				<p class="text-3xl font-semibold text-ufc-red">UFC Fight Night: Gane vs. Tuivasa</p>
-				<p class="text-gray-600 my-2">September 03, 2022</p>
-				<p class="text-lg">Paris, Ile-de-France, France</p>
+
+		{#each data as item}
+			<div class="rounded-lg shadow bg-gray-300 p-10 mt-48 relative">
+				<img src={item.fighter_1} alt="" class="h-80 absolute bottom-0 left-0" />
+				<div class="text-center">
+					<p class="text-3xl font-semibold text-ufc-red">{item.name}</p>
+					<p class="text-gray-600 my-2">{item.date}</p>
+					<p class="text-lg">{item.venue}</p>
+				</div>
+				<img src={item.fighter_2} alt="" class="h-80 absolute bottom-0 right-0 -scale-x-100" />
 			</div>
-			<img
-				src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2022-03/024162f6-408f-4518-8855-f055df3f5da8%252FTUIVASA_TAI_L_02-12.png?itok=3oinmKUW"
-				alt=""
-				class="h-80 absolute bottom-0 right-0 -scale-x-100"
-			/>
-		</div>
+		{/each}
 	</div>
 </main>
